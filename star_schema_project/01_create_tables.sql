1a68aceee6dd92faec8f35bfd57a32d3d3e0a109
Constantine Giakallis <58215516+Feasao@users.noreply.github.com>
2026-09-22 13:36:27 +0300

    Fix a typo in DROP statement for job postings

--- star_schema_project/01_create_tables.sql
+++ star_schema_project/01_create_tables.sql
@@ -1,7 +1,7 @@
 --Idempotent Creates
 
 DROP TABLE IF EXISTS skills_job_dim;
-DROP TABLE IF EXISTS jobs_postings_fact;
+DROP TABLE IF EXISTS job_postings_fact;
 DROP TABLE IF EXISTS company_dim;
 DROP TABLE IF EXISTS skills_dim;
 
